61f36ddaab9def4db1323251b262bcc29b384121
Valerian Coelho <valerian27012004@gmail.com>
2023-08-31 23:07:58 +0530

    Comments Added

--- Experiment 03/water_jug.py
+++ Experiment 03/water_jug.py
@@ -1,24 +1,32 @@
 import math
 
-mc = int(input('Enter the capacity of m : '))
-nc = int(input('Enter the capacity of n : '))
-d = int(input('Enter the capacity of d: '))
+# Get container capacities and desired amount from user input
+mc = int(input('Enter the capacity of m : '))  # Capacity of Container M
+nc = int(input('Enter the capacity of n : '))  # Capacity of Container N
+d = int(input('Enter the desired amount: '))   # Desired amount to achieve in both containers
 
-m = 0
-n = 0
+# Initialize container levels
+m = 0  # Current level of Container M
+n = 0  # Current level of Container N
 
-if(d % math.gcd(mc, nc) == 0):
+# Check if the desired amount is achievable using given containers
+if d % math.gcd(mc, nc) == 0:
     while (m != d and n != d):
-        if(m == 0):
+        # If Container M is empty, fill it to its capacity
+        if m == 0:
             m = mc
-        elif(n == nc):
+        # If Container N is full, empty it
+        elif n == nc:
             n = 0
         else:
+            # Transfer as much as possible from M to N
             transfer = min(m, nc - n)
-            m = m - transfer
-            n = n + transfer
+            m -= transfer
+            n += transfer
         
-        print(f'({m}, {n})')
+        # Print current container levels
+        print(f'Container M: {m}, Container N: {n}')
 
+    print('Desired amount achieved!')
 else:
     print('No solution')
